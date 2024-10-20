45934e68d6e535e6295148917db918349574d44c
Admin12121 <vickytaj6459@gmail.com>
2024-10-20 15:21:09 +0545

    profile updated

---
+++ src/pages/profile/Feed.tsx
@@ -0,0 +1,137 @@
+import InfiniteScrollContainer from "@/components/global/infinite-scroll";
+import Post, { Reposted } from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+import supabase from "@/api/feature/sessionProvider";
+import { Loader2 } from "lucide-react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { usePosts } from "../main/_components/postProvider";
+
+interface Profile {
+  profile?: {
+    id?: string;
+    email?: string;
+  };
+}
+
+export default function ForYouFeed(profile: Profile) {
+  const { id } = useParams();
+  const [posts, setPosts] = useState<any>([]);
+  const { refetch } = usePosts();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(0);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [user, setUser] = useState<any>({});
+
+  useEffect(() => {
+    async function getUserData() {
+      const { data, error } = await supabase.auth.getUser();
+      if (error) {
+        setUser(null);
+      } else {
+        setUser(data.user);
+      }
+    }
+    getUserData();
+  }, []);
+
+  const fetchPosts = async (pageParam = 0) => {
+    const isOtherUser = profile.profile?.id || (id && id.includes("@"));
+    const userId = isOtherUser ? profile.profile?.id : user.id;
+    const userEmail = isOtherUser ? profile.profile?.email : user.email;
+
+    if (userId && userEmail) {
+      try {
+        setLoading(true);
+
+        const { data: newPosts, error: postsError } = await supabase
+          .from('Posts')
+          .select(`*, profiles (id, email, full_name, picture)`)
+          .eq('created_by', userEmail)
+          .order('created_at', { ascending: false })
+          .range(pageParam * 5, (pageParam + 1) * 5 - 1);
+
+        if (postsError) throw new Error(postsError.message);
+
+        const { data: repostedPosts, error: repostsError } = await supabase
+          .from('repostead')
+          .select(`*, Posts (*, profiles (id, email, full_name, picture)), profiles (id, email, full_name, picture)`)
+          .eq('repostead_by', userId)
+          .order('created_at', { ascending: false })
+          .range(pageParam * 5, (pageParam + 1) * 5 - 1);
+
+        if (repostsError) throw new Error(repostsError.message);
+
+        const combinedPosts = [...newPosts, ...repostedPosts];
+
+        setPosts((prevPosts: any) => {
+          const postKeys = new Set(prevPosts.map((post: any) => {
+            return post.repostead_by ? `repost-${post.id}` : `post-${post.id}`;
+          }));
+
+          const uniqueNewPosts = combinedPosts.filter(post => {
+            const key = post.repostead_by ? `repost-${post.id}` : `post-${post.id}`;
+            return !postKeys.has(key);
+          });
+
+          return [...prevPosts, ...uniqueNewPosts];
+        });
+
+        setHasNextPage(newPosts.length === 5 || repostedPosts.length === 5);
+      } catch (err) {
+        setError((err as any).message);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if ((profile.profile?.id || (id && id.includes("@"))) || (!profile.profile && user && user.email)) {
+      fetchPosts(page);
+    }
+  }, [page, refetch, profile, user]);
+
+  const handleBottomReached = () => {
+    if (hasNextPage && !loading) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  if (loading && page === 0) {
+    return <PostsLoadingSkeleton />;
+  }
+
+  if (error) {
+    return (
+      <p className="text-center text-destructive">
+        An error occurred while loading posts: {error}
+      </p>
+    );
+  }
+
+  if (!posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        No any post yet.
+      </p>
+    );
+  }
+
+  return (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={handleBottomReached}
+    >
+      {posts.map((post: any) => (
+        post.repostead_by ? (
+          <Reposted key={Math.random()} post={post} />
+        ) : (
+          <Post key={Math.random()} post={post} />
+        )
+      ))}
+      {loading && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
+  );
+}
